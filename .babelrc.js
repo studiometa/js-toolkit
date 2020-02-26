99e223087b5085bfe30db75a994f6b82cdfc9b31
Titouan Mathis <titouan@studiometa.fr>
2020-02-26 22:52:15 +0100

    Add a `Base` class

---
+++ .babelrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: ["@babel/preset-env"],
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-proposal-class-properties",
+  ],
+  parserOpts: {
+    plugins: ["dynamicImport", "classProperties"]
+  },
+};
