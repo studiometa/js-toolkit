1934e46446ccd1ab4be347da21ab2cda2bee454a
Titouan Mathis <titouan@studiometa.fr>
2020-04-07 17:05:20 +0200

    Add breakpoints data to the `resize` service

--- dist/services/resize.js
+++ dist/services/resize.js
@@ -11,11 +11,15 @@ var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/cl
 
 var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
 
+var _assertThisInitialized2 = _interopRequireDefault(require("@babel/runtime/helpers/assertThisInitialized"));
+
 var _possibleConstructorReturn2 = _interopRequireDefault(require("@babel/runtime/helpers/possibleConstructorReturn"));
 
 var _getPrototypeOf2 = _interopRequireDefault(require("@babel/runtime/helpers/getPrototypeOf"));
 
 var _inherits2 = _interopRequireDefault(require("@babel/runtime/helpers/inherits"));
+
+var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
 
 var _Service2 = _interopRequireDefault(require("../abstracts/Service"));
 
@@ -42,8 +46,17 @@ var Resize = /*#__PURE__*/function (_Service) {
   var _super = _createSuper(Resize);
 
   function Resize() {
+    var _this;
+
     (0, _classCallCheck2["default"])(this, Resize);
-    return _super.apply(this, arguments);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _super.call.apply(_super, [this].concat(args));
+    (0, _defineProperty2["default"])((0, _assertThisInitialized2["default"])(_this), "breakpointElement", null);
+    return _this;
   }
 
   (0, _createClass2["default"])(Resize, [{
@@ -55,12 +68,13 @@ var Resize = /*#__PURE__*/function (_Service) {
      * @return {void}
      */
     value: function init() {
-      var _this = this;
+      var _this2 = this;
 
       this.handler = (0, _debounce["default"])(function () {
-        _this.trigger(_this.props);
+        _this2.trigger(_this2.props);
       }).bind(this);
       window.addEventListener('resize', this.handler);
+      this.breakpointElement = document.querySelector('[data-breakpoint]') || null;
     }
     /**
      * Unbind the handler from the resize event.
@@ -97,7 +111,33 @@ var Resize = /*#__PURE__*/function (_Service) {
         props.orientation = 'portrait';
       }
 
+      if (this.breakpointElement) {
+        props.breakpoint = this.breakpoint;
+        props.breakpoints = this.breakpoints;
+      }
+
       return props;
+    }
+    /**
+     * Get the current breakpoint.
+     * @return {String}
+     */
+
+  }, {
+    key: "breakpoint",
+    get: function get() {
+      return window.getComputedStyle(this.breakpointElement, '::before').getPropertyValue('content').replace(/"/g, '') || '';
+    }
+    /**
+     * Get all breakpoints.
+     * @return {Array}
+     */
+
+  }, {
+    key: "breakpoints",
+    get: function get() {
+      var breakpoints = window.getComputedStyle(this.breakpointElement, '::after').getPropertyValue('content').replace(/"/g, '');
+      return breakpoints.split(',');
     }
   }]);
   return Resize;
