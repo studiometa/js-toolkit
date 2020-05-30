adfcf4686204e0b03527ae42e9540c8e4d407639
Titouan Mathis <titouan@studiometa.fr>
2020-05-30 14:48:52 +0200

    Force the ouptut of the build to the CommonJS format

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
-  plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-export-default-from'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: 'cjs',
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
+  ],
   parserOpts: {
     plugins: ['dynamicImport', 'classProperties'],
   },
