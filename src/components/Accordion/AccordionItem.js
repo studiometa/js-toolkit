f7e346d7fc56bcd77af2183a2651ddd8de840add
Titouan Mathis <titouan@studiometa.fr>
2020-08-04 22:03:29 +0200

    Improve consistency of the Accordion styles options
    Use the same logic as the Modal component to declare the refs styles
    between states. This enables to add custom ref style without needing to
    extend the component's class.

--- src/components/Accordion/AccordionItem.js
+++ src/components/Accordion/AccordionItem.js
@@ -1,6 +1,6 @@
 import Base from '../../abstracts/Base';
-import * as classes from '../../utils/css/classes';
-import transition from '../../utils/css/transition';
+import * as styles from '../../utils/css/styles';
+import transition, { setClassesOrStyles } from '../../utils/css/transition';
 
 /**
  * AccordionItem class.
@@ -13,9 +13,13 @@ export default class AccordionItem extends Base {
   get config() {
     return {
       name: 'AccordionItem',
-      active: '',
-      enterActive: '',
-      leaveActive: '',
+      styles: {
+        container: {
+          open: '',
+          active: '',
+          closed: '',
+        },
+      },
     };
   }
 
@@ -26,6 +30,7 @@ export default class AccordionItem extends Base {
   mounted() {
     this.$refs.btn.setAttribute('id', this.$id);
     this.$refs.content.setAttribute('aria-labelledby', this.$id);
+    styles.add(this.$refs.container, { visibility: 'invisible', height: 0 });
   }
 
   /**
@@ -55,16 +60,39 @@ export default class AccordionItem extends Base {
     this.isOpen = true;
     this.$refs.container.setAttribute('aria-hidden', 'false');
 
-    classes.remove(this.$refs.container, 'invisible');
-    const height = this.$refs.content.offsetHeight;
-    await transition(this.$refs.container, {
-      from: { height: 0 },
-      active: this.$options.enterActive || this.$options.active,
-      to: { height: `${height}px` },
-    });
+    styles.remove(this.$refs.container, { visibility: 'invisible' });
+    const { container, ...otherStyles } = this.$options.styles;
 
-    classes.remove(this.$refs.container, 'h-0');
-    classes.remove(this.$refs.content, 'absolute');
+    await Promise.all([
+      transition(this.$refs.container, {
+        from: { height: 0 },
+        active: container.active,
+        to: { height: `${this.$refs.content.offsetHeight}px` },
+      }).then(() => {
+        // Do nothing if the item has been closed before the end
+        if (!this.isOpen) {
+          return Promise.resolve();
+        }
+
+        styles.remove(this.$refs.content, { position: 'absolute' });
+        return Promise.resolve();
+      }),
+      ...Object.entries(otherStyles).map(([refName, { open, active, closed } = {}]) =>
+        transition(this.$refs[refName], {
+          from: closed,
+          active,
+          to: open,
+        }).then(() => {
+          // Do nothing if the item has been closed before the end
+          if (!this.isOpen) {
+            return Promise.resolve();
+          }
+
+          setClassesOrStyles(this.$refs[refName], open);
+          return Promise.resolve();
+        })
+      ),
+    ]);
   }
 
   /**
@@ -81,15 +109,40 @@ export default class AccordionItem extends Base {
 
     this.isOpen = false;
 
-    const height = this.$refs.content.offsetHeight;
-    classes.add(this.$refs.content, 'absolute');
-    await transition(this.$refs.container, {
-      from: { height: `${height}px` },
-      active: this.$options.leaveActive || this.$options.active,
-      to: 'h-0',
-    });
-    classes.add(this.$refs.container, 'h-0');
-    classes.add(this.$refs.container, 'invisible');
-    this.$refs.container.setAttribute('aria-hidden', 'true');
+    const height = this.$refs.container.offsetHeight;
+    styles.add(this.$refs.content, { position: 'absolute' });
+    const { container, ...otherStyles } = this.$options.styles;
+
+    await Promise.all([
+      transition(this.$refs.container, {
+        from: { height: `${height}px` },
+        active: container.active,
+        to: { height: 0 },
+      }).then(() => {
+        // Do nothing if the item has been re-opened before the end
+        if (this.isOpen) {
+          return Promise.resolve();
+        }
+
+        styles.add(this.$refs.container, { height: 0, visibility: 'invisible' });
+        this.$refs.container.setAttribute('aria-hidden', 'true');
+        return Promise.resolve();
+      }),
+      ...Object.entries(otherStyles).map(([refName, { open, active, closed } = {}]) =>
+        transition(this.$refs[refName], {
+          from: open,
+          active,
+          to: closed,
+        }).then(() => {
+          // Do nothing if the item has been re-opened before the end
+          if (this.isOpen) {
+            return Promise.resolve();
+          }
+
+          setClassesOrStyles(this.$refs[refName], closed);
+          return Promise.resolve();
+        })
+      ),
+    ]);
   }
 }
