558a7cea7442b4ce386e502423832f3963173e33
Titouan Mathis <titouan@studiometa.fr>
2020-09-16 22:33:11 +0200

    Fix the demo and doc builds

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   ...require('@studiometa/tailwind-config'),
   purge: ['src/index.html', 'src/**/*.js'],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
 };
