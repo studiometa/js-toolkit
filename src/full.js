9ce6f261200afe1e8db25b641936be1c7e2401f7
Titouan Mathis <titouan@studiometa.fr>
2020-10-09 17:28:14 +0200

    Add functions to define components without class
    Legacy projects might not be able to use the class extension mechanism.

--- src/full.js
+++ src/full.js
@@ -1,11 +1,10 @@
-import Base from './abstracts/Base';
+import { defineComponent, createBase } from './index';
 
-export { Base };
+export { defineComponent, createBase };
+export Base from './abstracts/Base';
 
 export * as abstracts from './abstracts';
 export * as components from './components';
 export * as decorators from './decorators';
 export * as services from './services';
 export * as utils from './utils';
-
-export default Base;
