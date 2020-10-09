69d1f721d128e86616446bec8cd34f71cf6031df
Titouan Mathis <titouan@studiometa.fr>
2020-10-09 11:38:52 +0200

    Add an entrypoint for the whole package

---
+++ src/full.js
@@ -0,0 +1,11 @@
+import Base from './abstracts/Base';
+
+export { Base };
+
+export * as abstracts from './abstracts';
+export * as components from './components';
+export * as decorators from './decorators';
+export * as services from './services';
+export * as utils from './utils';
+
+export default Base;
