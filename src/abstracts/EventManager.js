b27c141cbec69722f5ba1c667746523046bfc561
Titouan Mathis <titouan@studiometa.fr>
2021-01-17 15:36:07 +0100

    Build assets and fix a docblock type declaration

--- src/abstracts/EventManager.js
+++ src/abstracts/EventManager.js
@@ -15,7 +15,7 @@ export default class EventManager {
    * Bind a listener function to an event.
    *
    * @param  {String}   event    Name of the event.
-   * @param  {String}   listener Function to be called.
+   * @param  {Function} listener Function to be called.
    * @return {Function}          A function to unbind the listener.
    */
   $on(event, listener) {
@@ -33,7 +33,7 @@ export default class EventManager {
    * Unbind a listener function from an event.
    *
    * @param  {String}       event    Name of the event.
-   * @param  {String}       listener Function to be removed.
+   * @param  {Function}     listener Function to be removed.
    * @return {EventManager}          The current instance.
    */
   $off(event, listener) {
