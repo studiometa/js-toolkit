531a80ec62c4e92f25daa8d2f60ba71e1bfc6910
Titouan Mathis <titouan@studiometa.fr>
2021-01-29 14:39:49 +0100

    Fix tests

--- tests/utils/index.spec.js
+++ tests/utils/index.spec.js
@@ -4,13 +4,13 @@ describe('~/utils exports', () => {
   it('should export all scripts', () => {
     expect(Object.keys(utils)).toEqual([
       'debounce',
+      'focusTrap',
       'keyCodes',
       'nextFrame',
       'throttle',
       'css',
       'math',
       'object',
-      'focusTrap',
       'history',
     ]);
   });
