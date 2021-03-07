777654a1fd829f82e80558509ef2eca11b5b20a8
Titouan Mathis <titouan@studiometa.fr>
2021-03-07 10:57:52 +0100

    Add tests and docs for the Cursor component

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -4,6 +4,23 @@ import matrix from '../utils/css/matrix';
 
 /**
  * @typedef {import('../services/pointer').PointerServiceProps} PointerServiceProps
+ * @typedef {import('../abstracts/Base').BaseOptions} BaseOptions
+ */
+
+/**
+ * @typedef {Object} CursorOptions
+ * @property {string} growSelectors
+ * @property {string} shrinkSelectors
+ * @property {number} growTo
+ * @property {number} shrinkTo
+ * @property {number} translateDampFactor
+ * @property {number} growDampFactor
+ * @property {number} shrinkDampFactor
+ */
+
+/**
+ * @typedef {Object} CursorInterface
+ * @property {BaseOptions & CursorOptions} $options
  */
 
 /**
@@ -47,20 +64,39 @@ export default class Cursor extends Base {
     },
   };
 
+  /**
+   * @type {Number}
+   */
   x = 0;
 
+  /**
+   * @type {Number}
+   */
   y = 0;
 
+  /**
+   * @type {Number}
+   */
   scale = 0;
 
+  /**
+   * @type {Number}
+   */
   pointerX = 0;
 
+  /**
+   * @type {Number}
+   */
   pointerY = 0;
 
+  /**
+   * @type {Number}
+   */
   pointerScale = 0;
 
   /**
    * Mounted hook.
+   * @return {void}
    */
   mounted() {
     this.x = 0;
@@ -74,7 +110,11 @@ export default class Cursor extends Base {
 
   /**
    * Moved hook.
+   *
+   * @this {Cursor & CursorInterface}
+   *
    * @param {PointerServiceProps} options
+   * @return {void}
    */
   moved({ event, x, y, isDown }) {
     if (!this.$services.has('ticked')) {
@@ -87,7 +127,7 @@ export default class Cursor extends Base {
     let scale = 1;
 
     if (!event) {
-      this.scale = scale;
+      this.pointerScale = scale;
       return;
     }
 
@@ -112,6 +152,10 @@ export default class Cursor extends Base {
 
   /**
    * RequestAnimationFrame hook.
+   *
+   * @this {Cursor & CursorInterface}
+   *
+   * @return {void}
    */
   ticked() {
     this.x = damp(this.pointerX, this.x, this.$options.translateDampFactor);
