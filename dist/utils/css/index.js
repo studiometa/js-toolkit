d5fbeeaca21b0bb57a6a5a24edf960f3521d5c18
Titouan Mathis <titouan@studiometa.fr>
2021-03-07 11:31:38 +0100

    Remove the dist directory from the repository

--- dist/utils/css/index.js
+++
@@ -1,6 +0,0 @@
-import * as classes from "./classes.js";
-import * as styles from "./styles.js";
-import matrix from "./matrix.js";
-import transition from "./transition.js";
-export { classes, styles, matrix, transition };
-//# sourceMappingURL=index.js.map
--- dist/utils/math/round.js
+++
@@ -1,5 +0,0 @@
-export default function round(value) {
-  var decimals = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-  return Number(value.toFixed(decimals));
-}
-//# sourceMappingURL=round.js.map
