a57500362ec3a983373110be76858dfc731fb847
Jérémie Valdenaire <jeremie.v@studiometa.fr>
2021-04-12 15:35:29 +0200

    Correct the typing in JSdoc Blocks

--- packages/js-toolkit/utils/cache.js
+++ packages/js-toolkit/utils/cache.js
@@ -1,23 +1,47 @@
 /**
+ * @typedef {Object} CacheItem
+ * @property {number | boolean} expire
+ * @property {unknown} data
+ */
+
+/**
  * Cache Class
  */
 export default class Cache {
   /**
+   * The name of the cache.
+   * @type {string}
+   */
+  name;
+
+  /**
+   * The cache store.
+   * @type {{ [cacheKey:string]: CacheItem }}
+   */
+  store = {};
+
+  /**
+   * The TTL for any cache item, false for infinity.
+   * @type {number|boolean}
+   */
+  expire;
+
+  /**
    * Class constructor.
-   * @param   {String} options.name The name of the cache.
-   * @param   {number} options.ttl  The time to live of any cache item.
-   * @return  {Cache}               A new Cache instance.
+   * @param  {Object} options
+   * @param  {string} options.name The name of the cache.
+   * @param  {number} options.ttl
+   * The time to live in minutes of any cache item.
    */
   constructor({ name, ttl = 0 }) {
     this.name = name;
-    this.store = {};
     this.expire = ttl === 0 ? false : ttl * 1000 * 60;
   }
 
   /**
    * Get an item by key.
-   * @param  {String} key The key of the item.
-   * @return {*}          The saved item or undefined if its TTL has expired.
+   * @param  {string}  key The key of the item.
+   * @return {unknown}     The saved item or undefined if its TTL has expired.
    */
   get(key) {
     const now = Date.now();
@@ -36,6 +60,7 @@ export default class Cache {
   set(key, value) {
     const now = Date.now();
     this.store[key] = {
+      // @ts-ignore
       expire: this.expire === false ? false : now + this.expire,
       data: value,
     };
