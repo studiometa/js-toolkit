b5c43f7953b2fff7c312be88033dbaa5ae2290cd
Titouan Mathis <titouan@studiometa.fr>
2021-04-21 15:43:40 +0200

    Add the possibility to pass options to the deepmerge fonction

--- packages/js-toolkit/decorators/withExtraConfig.js
+++ packages/js-toolkit/decorators/withExtraConfig.js
@@ -1,18 +1,21 @@
 import merge from 'deepmerge';
 
 /**
+ * @typedef {import('deepmerge').Options} DeepmergeOptions
  * @typedef {import('../abstracts/Base').BaseConfig} BaseConfig
  * @typedef {import('../abstracts/Base').BaseComponent} BaseComponent
  */
 
 /**
- * IntersectionObserver decoration.
+ * Extends the configuration of an existing class.
+ *
  * @param {BaseComponent} BaseClass The Base class to extend.
  * @param {Partial<BaseConfig>} config Extra configuration to merge.
+ * @param {DeepmergeOptions} options Options for the `deepmerge` function. {@link https://github.com/TehShrike/deepmerge#options}
  * @return {BaseComponent}
  */
-export default (BaseClass, config = {}) => {
-  const newConfig = merge(BaseClass.config, config);
+export default (BaseClass, config = {}, options = {}) => {
+  const newConfig = merge(BaseClass.config, config, options);
 
   if (newConfig.name === BaseClass.config.name) {
     newConfig.name = `${BaseClass.config.name}WithExtraConfig`;
