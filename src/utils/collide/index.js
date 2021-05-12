0a2d2caf4583031d33ce33e74ee0f7dd2dc13ed9
Antoine Quatrelivre <antoine@quatrelivre.com>
2021-05-12 12:45:40 +0200

    Add `collidePointRect` and `collideRectRect` utils

---
+++ src/utils/collide/index.js
@@ -0,0 +1,7 @@
+/**
+ * Inspired by http://www.jeffreythompson.org/collision-detection/
+ */
+import collidePointRect from './collidePointRect';
+import collideRectRect from './collideRectRect';
+
+export { collidePointRect, collideRectRect };
