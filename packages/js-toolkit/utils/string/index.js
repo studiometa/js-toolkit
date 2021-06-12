0df934b27257d706c0847ed6681d0131836b1c22
Titouan Mathis <titouan@studiometa.fr>
2021-06-12 15:10:39 +0200

    Add recursive functions to remove characters

--- packages/js-toolkit/utils/string/index.js
+++ packages/js-toolkit/utils/string/index.js
@@ -1,19 +1,23 @@
 import withLeadingCharacters from './withLeadingCharacters.js';
 import withLeadingSlash from './withLeadingSlash.js';
-import withTrailingCharacters from './withTrailingCharacters.js';
-import withTrailingSlash from './withTrailingSlash.js';
 import withoutLeadingCharacters from './withoutLeadingCharacters.js';
+import withoutLeadingCharactersRecursive from './withoutLeadingCharactersRecursive.js';
 import withoutLeadingSlash from './withoutLeadingSlash.js';
 import withoutTrailingCharacters from './withoutTrailingCharacters.js';
+import withoutTrailingCharactersRecursive from './withoutTrailingCharactersRecursive.js';
 import withoutTrailingSlash from './withoutTrailingSlash.js';
+import withTrailingCharacters from './withTrailingCharacters.js';
+import withTrailingSlash from './withTrailingSlash.js';
 
 export {
   withLeadingCharacters,
   withLeadingSlash,
-  withTrailingCharacters,
-  withTrailingSlash,
   withoutLeadingCharacters,
+  withoutLeadingCharactersRecursive,
   withoutLeadingSlash,
   withoutTrailingCharacters,
+  withoutTrailingCharactersRecursive,
   withoutTrailingSlash,
+  withTrailingCharacters,
+  withTrailingSlash,
 };
