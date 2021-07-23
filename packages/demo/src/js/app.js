64dfae614f3ce77ea37287f078ebf1ec33ff3ed3
Titouan Mathis <titouan@studiometa.fr>
2021-07-23 23:39:22 +0200

    Add the Draggable component to the demo

--- packages/demo/src/js/app.js
+++ packages/demo/src/js/app.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-classes-per-file */
 import Base from '@studiometa/js-toolkit';
-import { Modal, Tabs, Accordion, Cursor } from '@studiometa/js-toolkit/components';
+import { Modal, Tabs, Accordion, Cursor, Draggable } from '@studiometa/js-toolkit/components';
 import { matrix } from '@studiometa/js-toolkit/utils/css';
 import withBreakpointObserver from '@studiometa/js-toolkit/decorators/withBreakpointObserver.js';
 import BreakpointManagerDemo from './components/BreakPointManagerDemo/index.js';
@@ -33,6 +33,7 @@ class App extends Base {
           })}`;
         }
       },
+      Draggable,
       Skew: () => import(/* webpackChunkName: "Skew" */ './components/Skew.js'),
       '[data-src]': () => import(/* webpackChunkName: "Lazyload" */ './components/Lazyload.js'),
       Modal: withBreakpointObserver(Modal),
