ae99cbfd6bbd4afc427b6ff5fd62287f042ccf9f
Lucas Ulmer <lucas.u@studiometa.fr>
2021-08-09 16:13:51 +0200

    Add a decorator to manage Vue inside a component

--- packages/demo/src/js/app.js
+++ packages/demo/src/js/app.js
@@ -10,7 +10,7 @@ import {
 import { matrix } from '@studiometa/js-toolkit/utils/css';
 import withBreakpointObserver from '@studiometa/js-toolkit/decorators/withBreakpointObserver.js';
 import BreakpointObserverDemo from './components/BreakpointObserverDemo.js';
-import MyComponent from './components/MyComponent.js';
+import MyVueComponent from './components/MyVueComponent.js';
 
 // Add the new icon ref
 Accordion.config.components.AccordionItem.config.refs.push('icon');
@@ -34,7 +34,7 @@ class App extends Base {
           app
         ),
       BreakpointObserverDemo,
-      MyComponent,
+      MyVueComponent,
       Cursor: class extends Cursor {
         static config = {
           ...Cursor.config,
