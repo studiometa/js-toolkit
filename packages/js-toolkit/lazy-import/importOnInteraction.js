e74a199b0973c0ab74223d59a2078d6fbaef0b5e
Titouan Mathis <titouan@studiometa.fr>
2021-08-26 15:27:04 +0200

    Allow using import helpers without the parent context

--- packages/js-toolkit/lazy-import/importOnInteraction.js
+++ packages/js-toolkit/lazy-import/importOnInteraction.js
@@ -11,26 +11,25 @@ import { getComponentElements } from '../abstracts/Base/children.js';
  *   The import function.
  * @param {string} nameOrSelector
  *   The name or selector for the component.
- * @param {typeof Base} parent
- *   The parent component.
  * @param {string|string[]} events
  *   The events to listen to to trigger the import.
+ * @param {typeof Base=} [parent]
+ *   The parent component.
  *
  * @return {Promise<Base>}
  */
-export default function importOnInteraction(fn, nameOrSelector, parent, events) {
+export default function importOnInteraction(fn, nameOrSelector, events, parent) {
   // eslint-disable-next-line no-param-reassign
   events = Array.isArray(events) ? events : [events];
 
   return new Promise((resolve) => {
-    const elements = getComponentElements(nameOrSelector, parent.$el);
+    const elements = getComponentElements(nameOrSelector, parent?.$el);
     const eventListenerOptions = {
       capture: true,
       passive: true,
       once: true,
     };
     const handler = () => {
-      console.log('importOnInteraction', nameOrSelector);
       fn().then(resolve);
     };
 
