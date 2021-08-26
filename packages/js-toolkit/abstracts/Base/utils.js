ff8b843952048ed7aa15e95b075128524ec3e070
Titouan Mathis <titouan@studiometa.fr>
2021-08-26 15:53:07 +0200

    Refactor the getComponentElements function

--- packages/js-toolkit/abstracts/Base/utils.js
+++ packages/js-toolkit/abstracts/Base/utils.js
@@ -115,3 +115,30 @@ export function callMethod(instance, method, ...args) {
 
   return instance;
 }
+
+/**
+ * Get a list of elements based on the name of a component.
+ *
+ * @param {String} nameOrSelector
+ *   The name or selector to used for this component.
+ * @param {HTMLElement|Document} element
+ *   The root element on which to query the selector, defaults to `document`.
+ * @return {Array<HTMLElement>}
+ *   A list of elements on which the component should be mounted.
+ */
+export function getComponentElements(nameOrSelector, element = document) {
+  const selector = `[data-component="${nameOrSelector}"]`;
+  let elements = [];
+
+  try {
+    elements = Array.from(element.querySelectorAll(selector));
+    // eslint-disable-next-line no-empty
+  } catch {}
+
+  // If no child component found with the default selector, try a classic DOM selector
+  if (elements.length === 0) {
+    elements = Array.from(element.querySelectorAll(nameOrSelector));
+  }
+
+  return elements;
+}
