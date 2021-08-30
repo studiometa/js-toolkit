f991f17d70da2423646306bd773b9209405bb8b9
Lucas Ulmer <lucas.u@studiometa.fr>
2021-08-30 09:50:06 +0200

    Improve the withVue decorator

--- packages/js-toolkit/decorators/withVue.js
+++ packages/js-toolkit/decorators/withVue.js
@@ -14,9 +14,6 @@ export default (BaseClass) =>
     static config = {
       ...(BaseClass.config || {}),
       name: `${BaseClass?.config?.name ?? ''}WithVue`,
-      options: {
-        ...(BaseClass?.config?.options || {}),
-      },
     };
 
     /**
@@ -25,29 +22,16 @@ export default (BaseClass) =>
     constructor(element) {
       super(element);
 
-      this.$vueConfig = this.constructor.vueConfig;
+      const { vueConfig } = this.constructor;
 
-      if (!this.$vueConfig) {
+      if (!vueConfig) {
         throw new Error('[withVue] You must define a `vueConfig` object.');
       }
 
-      if (!this.$vueConfig.components) {
-        throw new Error('[withVue] You must define a `component` key in vueConfig.');
-      }
-
-      if (typeof this.$vueConfig.components !== 'object') {
-        throw new Error('[withVue] `component` must be an object.');
-      }
-
-      if (!this.$vueConfig.render) {
+      if (typeof vueConfig.render !== 'function') {
         throw new Error('[withVue] You must define a `render` function in vueConfig.');
       }
 
-      if (typeof this.$vueConfig.render !== 'function') {
-        throw new Error('[withVue] `render` must be a function.');
-      }
-
-      const vueConfig = this.$vueConfig;
       this.$vue = new Vue(vueConfig);
 
       this.$on('mounted', () => {
