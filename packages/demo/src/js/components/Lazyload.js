f6d46904c6f64e5660e1a8c2e81db06e057ee409
Titouan Mathis <titouan@studiometa.fr>
2021-09-10 16:36:14 +0200

    Fix tests

--- packages/demo/src/js/components/Lazyload.js
+++ packages/demo/src/js/components/Lazyload.js
@@ -24,7 +24,7 @@ export default class Lazyload extends withIntersectionObserver(Base) {
 
   intersected([entry]) {
     if (entry.isIntersecting) {
-      window.requestIdleCallback(this.load);
+      window.requestIdleCallback(() => this.load());
     }
   }
 
