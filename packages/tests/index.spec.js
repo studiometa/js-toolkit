59a7a9f07622b38457d03f4802fdb4b2ca2453c6
Titouan Mathis <titouan@studiometa.fr>
2021-09-10 16:36:14 +0200

    Fix tests

--- packages/tests/index.spec.js
+++ packages/tests/index.spec.js
@@ -1,22 +1,28 @@
 import { Base } from '@studiometa/js-toolkit';
-import * as toolkit from '@studiometa/js-toolkit/';
+import * as toolkit from '@studiometa/js-toolkit';
 
 describe('The package exports', () => {
   it('should export helpers and the Base class', () => {
     const names = [
       'Base',
       'createApp',
-      'createBase',
-      'default',
-      'defineComponent',
       'importOnInteraction',
       'importWhenIdle',
       'importWhenVisible',
+      'useDrag',
+      'useKey',
+      'useLoad',
+      'usePointer',
+      'useRaf',
+      'useResize',
+      'useScroll',
+      'withBreakpointManager',
+      'withBreakpointObserver',
+      'withDrag',
+      'withExtraConfig',
+      'withIntersectionObserver',
+      'withMountWhenInView',
     ];
     expect(Object.keys(toolkit)).toEqual(names);
-  });
-
-  it('should export the Base class as default export', () => {
-    expect(toolkit.default).toBe(Base);
   });
 });
