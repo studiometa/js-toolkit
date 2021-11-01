fba14389a2f8fa52943ebefae1c09196bec7b14f
Titouan Mathis <titouan@studiometa.fr>
2021-11-01 15:54:47 +0100

    Add a `precision` parameters to the `damp` function

--- packages/js-toolkit/utils/math/damp.js
+++ packages/js-toolkit/utils/math/damp.js
@@ -1,13 +1,14 @@
 /**
  * Get the next damped value for a given factor.
  *
- * @param  {Number} targetValue The final value.
- * @param  {Number} currentValue The current value.
- * @param  {Number=} [factor=0.5] The factor used to reach the target value.
- * @return {Number} The next value.
+ * @param  {number} targetValue The final value.
+ * @param  {number} currentValue The current value.
+ * @param  {number=} [factor=0.5] The factor used to reach the target value.
+ * @param  {number=} [precision=0.01] The precision used to calculate the latest value.
+ * @return {number} The next value.
  */
-export default function damp(targetValue, currentValue, factor = 0.5) {
-  return Math.abs(targetValue - currentValue) < 0.001
+export default function damp(targetValue, currentValue, factor = 0.5, precision = 0.01) {
+  return Math.abs(targetValue - currentValue) < precision
     ? targetValue
     : currentValue + (targetValue - currentValue) * factor;
 }
