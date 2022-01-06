bd837b19927c29dbefbab7fabf818e0f4b0c402f
Titouan Mathis <titouan@studiometa.fr>
2022-01-06 17:36:43 +0100

    Improve scroll service performance

--- packages/js-toolkit/services/index.js
+++ packages/js-toolkit/services/index.js
@@ -7,13 +7,16 @@ import useResize from './resize.js';
 import useScroll from './scroll.js';
 
 /**
+ * @template T
  * @typedef {Object} ServiceInterface
  * @property {(key:String) => void} remove
  *   Remove a function from the resize service by its key.
- * @property {(key:String, cb: (props: Object) => void) => Boolean} add
+ * @property {(key:String, cb: (props: T) => void) => Boolean} add
  *   Add a callback to the service. The callback will receive the current service props as parameter.
  * @property {(key:String) => Boolean} has
  *   Test if the service has alreaydy a callback for the given key.
+ * @property {() => T} props
+ *   Get the service's props.
  */
 
 export { useKey, usePointer, useDrag, useLoad, useRaf, useResize, useScroll };
