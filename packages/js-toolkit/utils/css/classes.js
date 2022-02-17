e391b1649b3f5db58eb105268ce02e9920115e66
Titouan Mathis <titouan@studiometa.fr>
2022-02-17 11:32:10 +0100

    Improve types for the `transition` utility function

--- packages/js-toolkit/utils/css/classes.js
+++ packages/js-toolkit/utils/css/classes.js
@@ -2,24 +2,28 @@
  * Manage a list of classes as string on an element.
  *
  * @param {HTMLElement} element The element to update.
- * @param {String} classNames A string of class names.
- * @param {String=} [method='add'] The method to use: add, remove or toggle.
+ * @param {string|string[]} classNames A string of class names.
+ * @param {'add'|'remove'|'toggle'} [method='add'] The method to use: add, remove or toggle.
  */
 function setClasses(element, classNames, method = 'add') {
   if (!element || !classNames) {
     return;
   }
 
-  classNames.split(' ').forEach((className) => {
-    element.classList[method](className);
-  });
+  const normalizedClassNames = Array.isArray(classNames) ? classNames : classNames.split(' ');
+
+  if (method !== 'toggle') {
+    element.classList[method](...normalizedClassNames);
+  } else {
+    normalizedClassNames.forEach((className) => element.classList[method](className));
+  }
 }
 
 /**
  * Add class names to an element.
  *
- * @param {HTMLElement} element    The element to update.
- * @param {String}      classNames A string of class names.
+ * @param  {HTMLElement}     element    The element to update.
+ * @param  {string|string[]} classNames A string of class names.
  * @return {void}
  */
 export function add(element, classNames) {
@@ -29,8 +33,8 @@ export function add(element, classNames) {
 /**
  * Remove class names from an element.
  *
- * @param  {HTMLElement} element    The element to update.
- * @param  {String}      classNames A string of class names.
+ * @param  {HTMLElement}     element    The element to update.
+ * @param  {string|string[]} classNames A string of class names.
  * @return {void}
  */
 export function remove(element, classNames) {
@@ -40,8 +44,8 @@ export function remove(element, classNames) {
 /**
  * Toggle class names from an element.
  *
- * @param  {HTMLElement} element    The element to update.
- * @param  {String}      classNames A string of class names.
+ * @param  {HTMLElement}     element    The element to update.
+ * @param  {string|string[]} classNames A string of class names.
  * @return {void}
  */
 export function toggle(element, classNames) {
