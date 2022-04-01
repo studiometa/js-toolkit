ac2de3ec36eec9f9d39e52dc536123d116898496
Titouan Mathis <titouan@studiometa.fr>
2022-04-01 17:02:15 +0200

    Lint files

--- packages/js-toolkit/helpers/getInstanceFromElement.js
+++ packages/js-toolkit/helpers/getInstanceFromElement.js
@@ -1,12 +1,11 @@
 /**
  * @typedef {import('../Base/index.js').BaseConstructor} BaseConstructor
- * @typedef {import('../Base/index.js').default} Base
  */
 
 /**
  * Get a component instance from a DOM element.
  * @template {BaseConstructor} T
- * @param    {HTMLElement & { __base__?: WeakMap<BaseConstructor, Base> }} element
+ * @param    {HTMLElement & { __base__?: WeakMap<T, InstanceType<T>> }} element
  * @param    {T} Constructor
  * @returns  {null|InstanceType<T>}
  */
