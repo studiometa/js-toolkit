a12b25287e88e8944f4a0d08a2e308537cc5ba2f
Titouan Mathis <titouan@studiometa.fr>
2022-04-05 16:52:14 +0200

    Update tests

--- packages/tests/decorators/withScrolledInView.spec.js
+++ packages/tests/decorators/withScrolledInView.spec.js
@@ -10,40 +10,107 @@ import wait from '../__utils__/wait.js';
 beforeAll(() => beforeAllCallback());
 
 let div;
-let instance;
 let divRectSpy;
+let offsetTopSpy;
+let offsetLeftSpy;
+let offsetWidthSpy;
+let offsetHeightSpy;
 const fn = jest.fn();
-
-class Foo extends withScrolledInView(Base) {
-  static config = {
-    name: 'Foo',
-  };
-
-  scrolledInView(props) {
-    fn(props);
-  }
-}
 
 beforeEach(() => {
   fn.mockClear();
+  window.pageYOffset = 0;
+  window.pageXOffset = 0;
   div = document.createElement('div');
+  offsetTopSpy = jest.spyOn(div, 'offsetTop', 'get').mockImplementation(() =>  500);
+  offsetLeftSpy = jest.spyOn(div, 'offsetLeft', 'get').mockImplementation(() =>  500);
+  offsetWidthSpy = jest.spyOn(div, 'offsetWidth', 'get').mockImplementation(() =>  100);
+  offsetHeightSpy = jest.spyOn(div, 'offsetHeight', 'get').mockImplementation(() =>  100);
   divRectSpy = jest.spyOn(div, 'getBoundingClientRect');
   divRectSpy.mockImplementation(() => ({
     height: 100,
     width: 100,
     top: 500,
+    y: 500,
     left: 500,
+    x: 500,
   }));
-  instance = new Foo(div);
 });
 
 afterEach(() => {
   afterEachCallback();
   divRectSpy.mockRestore();
+  offsetTopSpy.mockRestore();
+  offsetLeftSpy.mockRestore();
+  offsetWidthSpy.mockRestore();
+  offsetHeightSpy.mockRestore();
 });
 
 describe('The withScrolledInView decorator', () => {
   it('should trigger the `scrolledInView` hook when in view', async () => {
+    class Foo extends withScrolledInView(Base) {
+      static config = {
+        name: 'Foo',
+      };
+
+      scrolledInView(props) {
+        fn(props);
+      }
+    }
+    new Foo(div);
+
+    mockIsIntersecting(div, true);
+    const scrollHeightSpy = jest.spyOn(document.body, 'scrollHeight', 'get');
+    scrollHeightSpy.mockImplementation(() => window.innerHeight * 2);
+    const scrollWidthSpy = jest.spyOn(document.body, 'scrollWidth', 'get');
+    scrollWidthSpy.mockImplementation(() => window.innerWidth * 2);
+
+    document.dispatchEvent(new Event('scroll'));
+    window.pageYOffset = 10;
+    window.pageXOffset = 10;
+    document.dispatchEvent(new Event('scroll'));
+    window.pageYOffset *= 2;
+    window.pageXOffset *= 2;
+    document.dispatchEvent(new Event('scroll'));
+    window.pageYOffset *= 2;
+    window.pageXOffset *= 2;
+    document.dispatchEvent(new Event('scroll'));
+    window.pageYOffset *= 2;
+    window.pageXOffset *= 2;
+    document.dispatchEvent(new Event('scroll'));
+    window.pageYOffset *= 2;
+    window.pageXOffset *= 2;
+    document.dispatchEvent(new Event('scroll'));
+    window.pageYOffset *= 2;
+    window.pageXOffset *= 2;
+    document.dispatchEvent(new Event('scroll'));
+    window.pageYOffset *= 2;
+    window.pageXOffset *= 2;
+    document.dispatchEvent(new Event('scroll'));
+    window.pageYOffset *= 2;
+    window.pageXOffset *= 2;
+    document.dispatchEvent(new Event('scroll'));
+    window.pageYOffset *= 2;
+    window.pageXOffset *= 2;
+
+    await wait(50);
+    expect(fn.mock.calls.pop()).toMatchSnapshot();
+    scrollHeightSpy.mockRestore();
+    scrollWidthSpy.mockRestore();
+  });
+
+  it('should trigger the `scrolledInView` hook when in view with the `useOffsetSizes` option' , async () => {
+    class Foo extends withScrolledInView(Base, { useOffsetSizes: true }) {
+      static config = {
+        name: 'Foo',
+      };
+
+      scrolledInView(props) {
+        fn(props);
+      }
+    }
+    new Foo(div);
+
     mockIsIntersecting(div, true);
     const scrollHeightSpy = jest.spyOn(document.body, 'scrollHeight', 'get');
     scrollHeightSpy.mockImplementation(() => window.innerHeight * 2);
