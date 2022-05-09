a82fb7eff9b23021afec1fe9ad3f711e268a8d77
Titouan Mathis <titouan@studiometa.fr>
2022-05-09 22:23:29 +0200

    Improve demo

---
+++ packages/demo/src/js/components/AnimateScrollTest.js
@@ -0,0 +1,12 @@
+import { withScrolledInView } from '@studiometa/js-toolkit';
+import AnimateTest from './AnimateTest.js';
+
+export default class AnimateScrollTest extends withScrolledInView(AnimateTest) {
+  static config = {
+    name: 'AnimateScrollTest',
+  };
+
+  scrolledInView(props) {
+    this.animate.progress(props.dampedProgress.y);
+  }
+}
