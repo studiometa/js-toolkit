studiometa/js-toolkit

commit e76a592ce2b00a24cf595b2fe8634b72e35e0b1a

Fix `withScrolledInView` decorator usage

packages/js-toolkit/decorators/withScrolledInView.js
@@ -66,72 +66,86 @@ export default function withScrolledInView(BaseClass, options = {}) {
     dampPrecision = 0.001;
 
     /**
-     * Mounted hook.
-     * @returns {void}
+     * Bind listeners.
+     * @param   {HTMLElement} element
      */
-    mounted() {
-      this.__setProps();
-    }
+    constructor(element) {
+      super(element);
 
-    /**
-     * Resized hook.
-     * @returns {void}
-     */
-    resized() {
-      this.__setProps();
-    }
+      const delegate = {
+        handleEvent(event) {
+          delegate[event.type](event.detail[0]);
+        },
+        resized: () => {
+          this.__setProps();
+        },
+        scrolled: (props) => {
+          if ((!this.$services.has('ticked') && props.changed.y) || props.changed.x) {
+            this.$services.enable('ticked');
+          }
+        },
+        ticked: () => {
+          // X axis
+          this.__props.current.x = clamp(
+            window.pageXOffset,
+            this.__props.start.x,
+            this.__props.end.x
+          );
+          this.__props.progress.x = clamp01(
+            (this.__props.current.x - this.__props.start.x) /
+              (this.__props.end.x - this.__props.start.x)
+          );
+          this.__props.dampedProgress.x = damp(
+            this.__props.progress.x,
+            this.__props.dampedProgress.x,
+            this.dampFactor,
+            this.dampPrecision
+          );
 
-    /**
-     * Scrolled hook.
-     * @param   {any} props
-     * @returns {void}
-     */
-    scrolled(props) {
-      if ((!this.$services.has('ticked') && props.changed.y) || props.changed.x) {
-        this.$services.enable('ticked');
-      }
-    }
+          // Y axis
+          this.__props.current.y = clamp(
+            window.pageYOffset,
+            this.__props.start.y,
+            this.__props.end.y
+          );
+          this.__props.progress.y = clamp01(
+            (this.__props.current.y - this.__props.start.y) /
+              (this.__props.end.y - this.__props.start.y)
+          );
+          this.__props.dampedProgress.y = damp(
+            this.__props.progress.y,
+            this.__props.dampedProgress.y,
+            this.dampFactor,
+            this.dampPrecision
+          );
 
-    /**
-     * Raf hook.
-     * @returns {void}
-     */
-    ticked() {
-      // X axis
-      this.__props.current.x = clamp(window.pageXOffset, this.__props.start.x, this.__props.end.x);
-      this.__props.progress.x = clamp01(
-        (this.__props.current.x - this.__props.start.x) /
-          (this.__props.end.x - this.__props.start.x)
-      );
-      this.__props.dampedProgress.x = damp(
-        this.__props.progress.x,
-        this.__props.dampedProgress.x,
-        this.dampFactor,
-        this.dampPrecision
-      );
+          if (
+            this.__props.dampedProgress.x === this.__props.progress.x &&
+            this.__props.dampedProgress.y === this.__props.progress.y
+          ) {
+            this.$services.disable('ticked');
+          }
 
-      // Y axis
-      this.__props.current.y = clamp(window.pageYOffset, this.__props.start.y, this.__props.end.y);
-      this.__props.progress.y = clamp01(
-        (this.__props.current.y - this.__props.start.y) /
-          (this.__props.end.y - this.__props.start.y)
-      );
-      this.__props.dampedProgress.y = damp(
-        this.__props.progress.y,
-        this.__props.dampedProgress.y,
-        this.dampFactor,
-        this.dampPrecision
-      );
+          // @ts-ignore
+          this.__callMethod('scrolledInView', this.__props);
+        },
+      };
 
-      if (
-        this.__props.dampedProgress.x === this.__props.progress.x &&
-        this.__props.dampedProgress.y === this.__props.progress.y
-      ) {
-        this.$services.disable('ticked');
-      }
+      this.$on('before-mounted', () => {
+        this.$on('resized', delegate);
+        this.$on('scrolled', delegate);
+        this.$on('ticked', delegate);
+      });
 
-      // @ts-ignore
-      this.__callMethod('scrolledInView', this.__props);
+      this.$on('mounted', () => {
+        this.__setProps();
+      });
+
+      this.$on('destroyed', () => {
+        this.$off('resized', delegate);
+        this.$off('scrolled', delegate);
+        this.$off('ticked', delegate);
+      });
     }
 
     /**

packages/js-toolkit/Base/index.js
@@ -195,6 +195,7 @@ export default class Base extends EventTarget {
     name: 'Base',
     emits: [
       // hook events
+      'before-mounted',
       'mounted',
       'updated',
       'destroyed',
@@ -385,6 +386,8 @@ export default class Base extends EventTarget {
     if (this.$isMounted) {
       return this;
     }
+
+    this.$emit('before-mounted');
 
     this.$isMounted = true;
     if (isDev) {

commit dfbb7c4209dd79b53aa4c55d5c16a1c95bcc4ff6

Fix breaking change introduced in e76a592
Deleting the hook methods would have cause existing components calling
the methods via the `super` keyword to throw an error as the super
methods were not defined anymore. We instead deprecate the methods to
delete them in the next major release.

packages/js-toolkit/decorators/withScrolledInView.js
@@ -1,5 +1,5 @@
 import withMountWhenInView from './withMountWhenInView.js';
-import { damp, clamp, clamp01, getOffsetSizes } from '../utils/index.js';
+import { damp, clamp, clamp01, getOffsetSizes, isFunction } from '../utils/index.js';
 
 /**
  * @typedef {import('../Base').default} Base
@@ -146,6 +146,64 @@ export default function withScrolledInView(BaseClass, options = {}) {
         this.$off('scrolled', delegate);
         this.$off('ticked', delegate);
       });
+    }
+
+    /**
+     * Mounted hook.
+     *
+     * @deprecated
+     * @returns {void}
+     */
+    mounted() {
+      // @ts-ignore
+      if (isFunction(super.mounted)) super.mounted();
+    }
+
+    /**
+     * Resized hook.
+     *
+     * @deprecated
+     * @param   {import('../services/resize').ResizeServiceProps} props
+     * @returns {void}
+     */
+    resized(props) {
+      // @ts-ignore
+      if (isFunction(super.resized)) super.resized(props);
+    }
+
+    /**
+     * Scrolled hook.
+     *
+     * @deprecated
+     * @param   {import('../services/scroll').ScrollServiceProps} props
+     * @returns {void}
+     */
+    scrolled(props) {
+      // @ts-ignore
+      if (isFunction(super.scrolled)) super.scrolled(props);
+    }
+
+    /**
+     * Ticked hook.
+     *
+     * @deprecated
+     * @param   {import('../services/raf').RafServiceProps} props
+     * @returns {void}
+     */
+    ticked(props) {
+      // @ts-ignore
+      if (isFunction(super.ticked)) super.ticked(props);
+    }
+
+    /**
+     * Destroyed hook.
+     *
+     * @deprecated
+     * @returns {void}
+     */
+    destroyed() {
+      // @ts-ignore
+      if (isFunction(super.destroyed)) super.destroyed();
     }
 
     /**