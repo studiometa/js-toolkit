4a582a511d9efa413ebd3d9ffe21289fe39c11ce
Titouan Mathis <titouan@studiometa.fr>
2022-06-30 08:35:53 +0200

    Test const vs function

--- packages/js-toolkit/utils/is.js
+++ packages/js-toolkit/utils/is.js
@@ -2,23 +2,12 @@
 export const isDev = typeof __DEV__ !== 'undefined' && __DEV__;
 
 /**
- * Get the type of value.
- * @param   {unknown} value
- * @returns {string}
- */
-function type(value) {
-  return typeof value;
-}
-
-/**
  * Test if the given value is a function.
  *
  * @param   {unknown} value
- * @returns {fn is Function}
+ * @returns {value is Function}
  */
-export function isFunction(value) {
-  return type(value) === 'function';
-}
+export const isFunction = (value) => typeof value === 'function';
 
 /**
  * Test if a value is defined or not.
@@ -26,18 +15,14 @@ export function isFunction(value) {
  * @param   {unknown} value
  * @returns {boolean}
  */
-export function isDefined(value) {
-  return type(value) !== 'undefined';
-}
+export const isDefined = (value) => typeof value !== 'undefined';
 
 /**
  * Test if value is a string.
  * @param   {unknown}  value
  * @returns {value is string}
  */
-export function isString(value) {
-  return type(value) === 'string';
-}
+export const isString = (value) => typeof value === 'string';
 
 /**
  * Test if the given value is an object.
@@ -45,9 +30,8 @@ export function isString(value) {
  * @param {unknown} value
  * @returns {boolean}
  */
-export function isObject(value) {
-  return type(value) === 'object' && !!value && value.toString() === '[object Object]';
-}
+export const isObject = (value) =>
+  typeof value === 'object' && !!value && value.toString() === '[object Object]';
 
 /**
  * Test if a given value is a number.
@@ -55,17 +39,14 @@ export function isObject(value) {
  * @param   {unknown}  value
  * @returns {value is number}
  */
-export function isNumber(value) {
-  return type(value) === 'number';
-}
+export const isNumber = (value) => typeof value === 'number';
 
 /**
  * Test if a given value is a boolean.
  * @param   {unknown}  value
  * @returns {value is boolean}
  */
-export function isBoolean(value) {
-  return type(value) === 'boolean';
-}
+export const isBoolean = (value) => typeof value === 'boolean';
 
+// eslint-disable-next-line prefer-destructuring
 export const isArray = Array.isArray;
