60ef231d47a161b96611537ddef4c30a08aabc53
Titouan Mathis <titouan@studiometa.fr>
2022-07-19 15:38:44 +0200

    Improve export-size reports
    Add multiple exports to compare size between each part of the package.

--- scripts/add-utils-export.js
+++ scripts/add-utils-export.js
@@ -3,7 +3,30 @@ const fs = require('fs');
 
 const index = path.resolve(__dirname, '../packages/js-toolkit/index.js');
 
-let content = fs.readFileSync(index, { encoding: 'utf-8' });
-content += "\nexport * from './utils/index.js';\n";
+const content = `import Base from './Base/index.js';
+import * as DECORATORS from './decorators/index.js';
+import * as HELPERS from './helpers/index.js';
+import * as SERVICES from './services/index.js';
+import * as UTILS from './utils/index.js';
+
+export * from './decorators/index.js';
+export * from './helpers/index.js';
+export * from './services/index.js';
+export * from './utils/index.js';
+
+export { Base, DECORATORS, HELPERS, SERVICES, UTILS };
+
+export const FRAMEWORK = {
+  Base,
+  DECORATORS,
+  HELPERS,
+  SERVICES,
+};
+
+export const ALL = {
+  FRAMEWORK,
+  UTILS,
+};
+`;
 
 fs.writeFileSync(index, content, { encoding: 'UTF-8' });
