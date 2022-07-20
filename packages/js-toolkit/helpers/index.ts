862c08baf44e77c359e17f08c10f6adf25c6fdc7
Titouan Mathis <titouan@studiometa.fr>
2022-07-20 10:36:15 +0200

    Add a `getClosestParent` helper

--- packages/js-toolkit/helpers/index.ts
+++ packages/js-toolkit/helpers/index.ts
@@ -4,3 +4,4 @@ export { default as getInstanceFromElement } from './getInstanceFromElement.js';
 export { default as importOnInteraction } from './importOnInteraction.js';
 export { default as importWhenIdle } from './importWhenIdle.js';
 export { default as importWhenVisible } from './importWhenVisible.js';
+export { default as getClosestParent } from './getClosestParent.js';
