693c2bc02b9039a6afc164cddf5412e32fd135af
Titouan Mathis <titouan@studiometa.fr>
2022-08-18 21:54:05 +0200

    Lint files

--- packages/docs/.vitepress/config.js
+++ packages/docs/.vitepress/config.js
@@ -8,7 +8,8 @@ const pkg = JSON.parse(
 export default defineConfig({
   lang: 'en-US',
   title: 'JS Toolkit',
-  description: 'The JS Toolkit by Studio Meta is a JavaScript data-attributes driven micro-framework shipped with plenty of useful utility functions to boost your project.',
+  description:
+    'The JS Toolkit by Studio Meta is a JavaScript data-attributes driven micro-framework shipped with plenty of useful utility functions to boost your project.',
   lastUpdated: true,
   head: [['link', { rel: 'icon', type: 'image/x-icon', href: '/logo.png' }]],
   themeConfig: {
