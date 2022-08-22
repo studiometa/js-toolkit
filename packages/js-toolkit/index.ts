909e661bf8ce4a6a61a1a3cb4c5d8b251a2eb1a3
Titouan Mathis <titouan@studiometa.fr>
2022-08-22 18:35:58 +0200

    Fix exports

--- packages/js-toolkit/index.ts
+++ packages/js-toolkit/index.ts
@@ -1,7 +1,7 @@
 export { default as Base } from './Base/index.js';
 export * from './decorators/index.js';
-// export * from './helpers/index.js';
-// export * from './services/index.js';
+export * from './helpers/index.js';
+export * from './services/index.js';
 
 export type {
   BaseTypeParameter,
