70c27d88500052a7a086fb77108026fec6a849cc
Titouan Mathis <titouan@studiometa.fr>
2022-08-22 19:57:21 +0200

    Add a `BaseInterface` interface

---
+++ packages/js-toolkit/Base/types.ts
@@ -0,0 +1,51 @@
+import {
+  KeyServiceProps,
+  LoadServiceProps,
+  PointerServiceProps,
+  RafServiceProps,
+  ResizeServiceProps,
+  ScrollServiceProps,
+} from '../services/index.js';
+
+export interface BaseInterface {
+  /**
+   * Mounted hook.
+   */
+  mounted?(): void;
+  /**
+   * Updated hook.
+   */
+  updated?(): void;
+  /**
+   * Destroyed hook.
+   */
+  destroyed?(): void;
+  /**
+   * Terminated hook.
+   */
+  terminated?(): void;
+  /**
+   * Key service, executed when a keyboard key is pressed or released.
+   */
+  keyed?(props: KeyServiceProps): void;
+  /**
+   * Load service, executed when the window `load` event is fired.
+   */
+  loaded?(props: LoadServiceProps): void;
+  /**
+   * Pointer service, executed when the pointer is moving, pressed or released.
+   */
+  moved?(props: PointerServiceProps): void;
+  /**
+   * Raf service, executed on each rendered frame.
+   */
+  ticked?(props: RafServiceProps): void;
+  /**
+   * Resize service, executed when the window is resized.
+   */
+  resized?(props: ResizeServiceProps): void;
+  /**
+   * Scroll service, executed when the document is scrolled.
+   */
+  scrolled?(props: ScrollServiceProps): void;
+}
