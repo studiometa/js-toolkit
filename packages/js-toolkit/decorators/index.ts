63f1816c0189c8cb937ac9ddaf1b0e98972430f7
Titouan Mathis <titouan@studiometa.fr>
2022-08-30 20:34:20 +0200

    Fix properties definition

--- packages/js-toolkit/decorators/index.ts
+++ packages/js-toolkit/decorators/index.ts
@@ -6,6 +6,6 @@ export { default as withFreezedOptions } from './withFreezedOptions.js';
 export { default as withIntersectionObserver } from './withIntersectionObserver.js';
 export { default as withMountWhenInView } from './withMountWhenInView.js';
 export { default as withResponsiveOptions } from './withResponsiveOptions.js';
-export { default as withScrolledInView } from './withScrolledInView.js';
+export * from './withScrolledInView.js';
 export { default as withType } from './withType.js'
 export { default as withVue2 } from './withVue2.js';
