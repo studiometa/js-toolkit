2306ac6cf360cb418ed9057bf0d0ce1f209f223d
KeziahMoselle <apps@keziahmoselle.fr>
2022-11-07 20:05:43 +0100

    Add importOnMedia

--- packages/js-toolkit/helpers/index.ts
+++ packages/js-toolkit/helpers/index.ts
@@ -5,3 +5,4 @@ export { default as getInstanceFromElement } from './getInstanceFromElement.js';
 export { default as importOnInteraction } from './importOnInteraction.js';
 export { default as importWhenIdle } from './importWhenIdle.js';
 export { default as importWhenVisible } from './importWhenVisible.js';
+export { default as importOnMedia } from './importOnMedia.js';
