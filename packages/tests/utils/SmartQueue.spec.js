0dfcd5ac6da0ea9537e8b23bcf072a9ec08e0e19
Titouan Mathis <titouan@studiometa.fr>
2023-01-22 22:26:26 +0100

    Fix tests

---
+++ packages/tests/utils/SmartQueue.spec.js
@@ -0,0 +1,28 @@
+import { jest } from '@jest/globals';
+import { SmartQueue, nextTick } from '@studiometa/js-toolkit/utils';
+
+function task(duration = 1) {
+  const start = performance.now();
+  let now = performance.now();
+  while (now - start < duration) {
+    now = performance.now();
+  }
+}
+
+describe('The `SmartQueue` class', () => {
+  it('should run multiple functions in queue without triggering long tasks', async () => {
+    const queue = new SmartQueue();
+    const spy = jest.fn(task);
+
+    queue.add(() => spy(15));
+    queue.add(() => spy(15));
+    queue.add(() => spy(15));
+    queue.add(() => spy(15));
+
+    expect(spy).toHaveBeenCalledTimes(0);
+    await nextTick();
+    expect(spy).toHaveBeenCalledTimes(3);
+    await nextTick();
+    expect(spy).toHaveBeenCalledTimes(4);
+  });
+});
