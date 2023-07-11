6ea19052395af4fe872c8f46bd338498e7b3121b
Titouan Mathis <titouan@studiometa.fr>
2023-07-11 10:29:09 +0200

    Add a `withRelativePointer` decorator

---
+++ packages/js-toolkit/decorators/withRelativePointer.ts
@@ -0,0 +1,58 @@
+import type { BaseDecorator, BaseInterface } from '../Base/types.js';
+import type { Base, BaseProps, BaseConfig } from '../Base/index.js';
+import type { PointerServiceProps } from '../services/pointer.js';
+import { usePointer } from '../services/index.js';
+
+export type RelativePointerDecoratorOptions = {
+  target?: (this: Base, instance: Base) => HTMLElement;
+};
+
+export interface RelativePointerInterface extends BaseInterface {
+  movedRelatively?(props: PointerServiceProps): void;
+}
+
+/**
+ * Add dragging capabilities to a component.
+ */
+export function withRelativePointer<S extends Base>(
+  BaseClass: typeof Base,
+  { target = (instance) => instance.$el }: RelativePointerDecoratorOptions = {},
+): BaseDecorator<BaseInterface, S> {
+  /**
+   * Class.
+   */
+  class WithRelativePointer<T extends BaseProps = BaseProps> extends BaseClass<
+    T & RelativePointerInterface
+  > {
+    /**
+     * Config.
+     */
+    static config: BaseConfig = {
+      name: `${BaseClass.config.name}WithRelativePointer`,
+      emits: ['movedrelative'],
+    };
+
+    /**
+     * Class constructor.
+     */
+    constructor(element: HTMLElement) {
+      super(element);
+
+      this.$on('mounted', () => {
+        this.$services.register(
+          'movedrelative',
+          usePointer.bind(undefined, target.call(this, this)),
+        );
+        this.$services.enable('movedrelative');
+      });
+
+      this.$on('destroyed', () => {
+        this.$services.disable('movedrelative');
+        this.$services.unregister('movedrelative');
+      });
+    }
+  }
+
+  // @ts-ignore
+  return WithRelativePointer;
+}
