0723619488d49011c0d844d32e1d5c07ba646431
Titouan Mathis <titouan@studiometa.fr>
2023-08-01 09:39:33 +0200

    Improve performance

--- packages/js-toolkit/utils/string/endsWith.ts
+++ packages/js-toolkit/utils/string/endsWith.ts
@@ -6,13 +6,11 @@
  * @see https://jsbench.me/1hlkqqd0ff/2
  */
 export function endsWith(str: string, search: string): boolean {
-  const { length } = search;
-
-  if (length === 0) {
+  if (search.length === 0) {
     return true;
   }
 
-  if (length === 1) {
+  if (search.length === 1) {
     // eslint-disable-next-line unicorn/prefer-at
     return str[str.length - 1] === search;
   }
