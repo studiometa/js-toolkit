702f4713ceed1754e7e4fcfc049af2fc678f79ed
Titouan Mathis <titouan@studiometa.fr>
2023-12-01 11:56:26 +0100

    Update doc

--- packages/js-toolkit/utils/string/endsWith.ts
+++ packages/js-toolkit/utils/string/endsWith.ts
@@ -5,16 +5,16 @@
  *
  * @see https://jsbench.me/1hlkqqd0ff/2
  */
-export function endsWith(str: string, search: string): boolean {
+export function endsWith(string: string, search: string): boolean {
   if (search.length === 0) {
     return true;
   }
 
   if (search.length === 1) {
     // eslint-disable-next-line unicorn/prefer-at
-    return str[str.length - 1] === search;
+    return string[string.length - 1] === search;
   }
 
   // eslint-disable-next-line unicorn/prefer-string-slice
-  return str.substring(str.length - search.length) === search;
+  return string.substring(string.length - search.length) === search;
 }
--- packages/js-toolkit/utils/string/startsWith.ts
+++ packages/js-toolkit/utils/string/startsWith.ts
@@ -5,15 +5,15 @@
  *
  * @see https://jsbench.me/1hlkqqd0ff/1
  */
-export function startsWith(str: string, search: string): boolean {
+export function startsWith(string: string, search: string): boolean {
   if (search.length === 0) {
     return true;
   }
 
   if (search.length === 1) {
-    return str[0] === search;
+    return string[0] === search;
   }
 
   // eslint-disable-next-line unicorn/prefer-string-slice
-  return str.substring(0, search.length) === search;
+  return string.substring(0, search.length) === search;
 }
