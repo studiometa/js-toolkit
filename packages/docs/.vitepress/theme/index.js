66f30c6b27e7f61341d564d5a83e6a9a06e1f4ad
Titouan Mathis <titouan@studiometa.fr>
2024-01-10 18:10:28 +0100

    Remove unnecessary override for VitePress

--- packages/docs/.vitepress/theme/index.js
+++ packages/docs/.vitepress/theme/index.js
@@ -1,12 +1,10 @@
 import DefaultTheme from 'vitepress/theme';
-import Badge from './components/Badge.vue';
 import PreviewIframe from './components/PreviewIframe.vue';
 import './custom.scss';
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    app.component('Badge', Badge);
     app.component('PreviewIframe', PreviewIframe);
   },
 };
