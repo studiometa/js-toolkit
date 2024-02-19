94983f0a6a91b4c1c66475682ae413544843ab57
Titouan Mathis <titouan@studiometa.fr>
2024-02-19 13:55:23 +0100

    Improve types

--- packages/js-toolkit/helpers/createApp.ts
+++ packages/js-toolkit/helpers/createApp.ts
@@ -5,7 +5,7 @@ import { isDefined } from '../utils/index.js';
 
 export type CreateAppOptions = {
   root: HTMLElement;
-  features: Features;
+  features: Partial<Features>;
   breakpoints?: Record<string, string>;
 };
 
