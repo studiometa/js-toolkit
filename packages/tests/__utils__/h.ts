0a8874c86b0932364c9510c0dd2c696372fdd231
Titouan Mathis <titouan@studiometa.fr>
2024-02-19 20:55:00 +0100

    Update test

--- packages/tests/__utils__/h.ts
+++ packages/tests/__utils__/h.ts
@@ -3,12 +3,24 @@
  */
 export function h<T extends keyof HTMLElementTagNameMap = 'div'>(
   tag: T,
+  children?: (string | Node)[],
+): HTMLElementTagNameMap[T];
+export function h<T extends keyof HTMLElementTagNameMap = 'div'>(
+  tag: T,
+  attributes?: Record<string, string>,
+  children?: (string | Node)[],
+): HTMLElementTagNameMap[T];
+export function h<T extends keyof HTMLElementTagNameMap = 'div'>(
+  tag: T,
   attributes: Record<string, string> = {},
   children: (string | Node)[] = [],
 ): HTMLElementTagNameMap[T] {
   const el = document.createElement(tag);
-  for (const [name, value] of Object.entries(attributes)) {
-    el.setAttribute(name.replaceAll(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(), value);
+
+  if (!Array.isArray(attributes)) {
+    for (const [name, value] of Object.entries(attributes)) {
+      el.setAttribute(name.replaceAll(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(), value);
+    }
   }
 
   el.append(...children);
