56bf1f5381ae18f20289e1e635933afb528cd9b6
Titouan Mathis <titouan@studiometa.fr>
2024-02-19 11:49:07 +0100

    Remove JS tests

--- packages/tests/index.spec.js
+++
@@ -1,7 +0,0 @@
-import * as toolkit from '@studiometa/js-toolkit';
-
-describe('The package exports', () => {
-  it('should export helpers and the Base class', () => {
-    expect(Object.keys(toolkit)).toMatchSnapshot();
-  });
-});
