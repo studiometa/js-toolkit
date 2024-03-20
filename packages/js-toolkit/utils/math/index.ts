a089fac2ef12dfbc45704936765e74c3fb850eac
Titouan Mathis <titouan@studiometa.fr>
2024-03-20 14:36:19 +0100

    Add a `mean` utility function

--- packages/js-toolkit/utils/math/index.ts
+++ packages/js-toolkit/utils/math/index.ts
@@ -1,3 +1,6 @@
+export * as ease from './ease.js';
+export * from './createEases.js';
+export * from './ease.js';
 export { default as clamp } from './clamp.js';
 export { default as clamp01 } from './clamp01.js';
 export { default as damp } from './damp.js';
@@ -5,6 +8,4 @@ export { default as inertiaFinalValue } from './inertiaFinalValue.js';
 export { default as lerp } from './lerp.js';
 export { default as map } from './map.js';
 export { default as round } from './round.js';
-export * from './ease.js';
-export * as ease from './ease.js';
-export * from './createEases.js';
+export { mean } from './mean.js';
