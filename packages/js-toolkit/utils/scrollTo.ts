ee7491fc059631927628a0e0273bf05fba6a0cb5
Antoine Quatrelivre <antoine@studiometa.fr>
2024-03-27 19:43:20 +0100

    Update `scrollTo` with rootElement and axis options

--- packages/js-toolkit/utils/scrollTo.ts
+++ packages/js-toolkit/utils/scrollTo.ts
@@ -2,7 +2,19 @@ import { TweenOptions, tween } from './tween.js';
 import { isFunction } from './is.js';
 import { lerp } from './math/index.js';
 
-export type ScrollToOptions = TweenOptions & {
+// Rename ScrollToOptions type to ScrollPosition to be more understandable.
+type ScrollPosition = ScrollToOptions;
+
+// Set the type of TweenScrollToOptions options.
+export type TweenScrollToOptions = TweenOptions & {
+  /**
+   * Root element that will be scrolled.
+   */
+  rootElement?: HTMLElement;
+  /**
+   * Scroll direction.
+   */
+  axis?: 'X' | 'Y' | 'BOTH';
   /**
    * Distance from the target.
    */
@@ -10,71 +22,150 @@ export type ScrollToOptions = TweenOptions & {
 };
 
 /**
- * Handler for the click event on anchor links
+ * Scroll to an element.
  *
- * @returns {Promise<number>} A promising resolving with the target scroll position.
+ * @returns {Promise<ScrollPosition>} A promising resolving with the scroll position.
  */
 export default function scrollTo(
   selectorElement: HTMLElement | string,
-  { offset = 0, ...tweenOptions }: ScrollToOptions = {},
-): Promise<number> {
-  let targetElement = null;
-  const scroll = window.scrollY;
+  { rootElement = null, axis = 'Y', offset = 0, ...tweenOptions }: TweenScrollToOptions = {},
+): Promise<ScrollPosition> {
+  /**
+   * Get the current root element or window scroll position.
+   *
+   * @returns {ScrollPosition}
+   */
+  function getCurrentScrollPosition(): ScrollPosition {
+    return rootElement
+      ? {
+          left: rootElement.scrollTop,
+          top: rootElement.scrollLeft,
+        }
+      : {
+          left: window.scrollX,
+          top: window.scrollY,
+        };
+  }
 
+  // Save the current scroll position.
+  const initialScrollPosition = getCurrentScrollPosition();
+
+  // Init the target element.
+  let targetElement = null;
+
+  // Check if the given element is a selector or already a HTMLElement.
   if (selectorElement instanceof HTMLElement) {
     targetElement = selectorElement;
   } else {
     targetElement = document.querySelector(selectorElement);
   }
 
+  // Resolve the promise if there is no target element.
   if (!targetElement) {
-    return Promise.resolve(scroll);
+    return Promise.resolve(initialScrollPosition);
   }
 
+  // Get the size of the target element.
   const sizes = targetElement.getBoundingClientRect();
-  const scrollMargin = getComputedStyle(targetElement).scrollMarginTop || '0';
-  const max = document.documentElement.scrollHeight - window.innerHeight;
-  let scrollTarget = sizes.top + scroll - Number.parseInt(scrollMargin, 10) - offset;
 
-  // Make sure to not scroll more than the max scroll allowed
-  if (scrollTarget > max) {
-    scrollTarget = max;
-  }
+  // Get the scroll margins of the target element.
+  const targetElementComputedStyle = getComputedStyle(targetElement);
+  const scrollMargin = {
+    left: Number.parseInt(targetElementComputedStyle.scrollMarginLeft || '0', 10),
+    top: Number.parseInt(targetElementComputedStyle.scrollMarginTop || '0', 10),
+  };
 
+  // Get the maximum scroll of the root element or of the window.
+  const max = rootElement
+    ? {
+        left: rootElement.scrollWidth - rootElement.offsetWidth,
+        top: rootElement.scrollHeight - rootElement.offsetHeight,
+      }
+    : {
+        left: document.documentElement.scrollWidth - window.innerWidth,
+        top: document.documentElement.scrollHeight - window.innerHeight,
+      };
+
+  // Get the scroll position (or maximum scroll) of the target element.
+  const targetElementScrollPosition = {
+    left: Math.min(sizes.left + initialScrollPosition.left - scrollMargin.left - offset, max.left),
+    top: Math.min(sizes.top + initialScrollPosition.top - scrollMargin.top - offset, max.top),
+  };
+
+  // Create the final promise.
   return new Promise((resolve) => {
+    // Init tween.
     let tw = null;
 
+    // Set the event handler.
     const eventHandler = () => {
+      // Pause the tween animation.
       tw.pause();
 
-      window.removeEventListener('wheel', eventHandler);
-      window.removeEventListener('touchmove', eventHandler);
+      // Remove the event listeners.
+      if (rootElement) {
+        rootElement.removeEventListener('wheel', eventHandler);
+        rootElement.removeEventListener('touchmove', eventHandler);
+      } else {
+        window.removeEventListener('wheel', eventHandler);
+        window.removeEventListener('touchmove', eventHandler);
+      }
 
-      resolve(window.scrollY);
+      // Resolve the promise if the user interact with wheel or touchmove.
+      resolve(getCurrentScrollPosition());
     };
 
+    // Setup tween.
     tw = tween(
       (progress) => {
-        window.scrollTo(0, lerp(scroll, scrollTarget, progress));
+        // Scroll to the progress scroll position.
+        (rootElement ?? window).scrollTo({
+          left:
+            axis === 'X' || axis === 'BOTH'
+              ? lerp(initialScrollPosition.left, targetElementScrollPosition.left, progress)
+              : initialScrollPosition.left,
+          top:
+            axis === 'Y' || axis === 'BOTH'
+              ? lerp(initialScrollPosition.top, targetElementScrollPosition.top, progress)
+              : initialScrollPosition.top,
+        });
       },
       {
+        // Use tween options.
         ...tweenOptions,
-        onFinish(progress) {
-          window.removeEventListener('wheel', eventHandler);
-          window.removeEventListener('touchmove', eventHandler);
 
+        // Set finish actions.
+        onFinish(progress) {
+          // Remove the event listeners.
+          if (rootElement) {
+            rootElement.removeEventListener('wheel', eventHandler);
+            rootElement.removeEventListener('touchmove', eventHandler);
+          } else {
+            window.removeEventListener('wheel', eventHandler);
+            window.removeEventListener('touchmove', eventHandler);
+          }
+
+          // Run onFinish function passed in options.
           if (isFunction(tweenOptions.onFinish)) {
             tweenOptions.onFinish(progress);
           }
 
-          resolve(window.scrollY);
+          // Resolve the promise on animation finish.
+          resolve(getCurrentScrollPosition());
         },
       },
     );
 
-    window.addEventListener('wheel', eventHandler, { passive: true });
-    window.addEventListener('touchmove', eventHandler, { passive: true });
+    // Set the event listeners to catch user interaction
+    if (rootElement) {
+      rootElement.addEventListener('wheel', eventHandler, { passive: true });
+      rootElement.addEventListener('touchmove', eventHandler, { passive: true });
+    } else {
+      window.addEventListener('wheel', eventHandler, { passive: true });
+      window.addEventListener('touchmove', eventHandler, { passive: true });
+    }
 
+    // Start the tween animation.
     tw.start();
   });
 }
