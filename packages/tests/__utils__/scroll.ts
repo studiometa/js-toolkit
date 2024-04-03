f40dcb62b6af8e2a4c39384de6b572437ff2fe3d
Titouan Mathis <titouan@studiometa.fr>
2024-04-03 16:15:15 +0200

    Update tests

--- packages/tests/__utils__/scroll.ts
+++ packages/tests/__utils__/scroll.ts
@@ -1,18 +1,25 @@
 import { mock } from 'bun:test';
 
 let scrollHeight: number;
+let scrollWidth: number;
 
 export function mockScroll({ height = 0, width = 0 } = {}) {
   scrollHeight = document.documentElement.scrollHeight;
+  scrollWidth = document.documentElement.scrollWidth;
   const scrollHeightSpy = mock(() => height);
+  const scrollWidthSpy = mock(() => width);
   Object.defineProperties(document.documentElement, {
     scrollHeight: {
       configurable: true,
       get: () => scrollHeightSpy(),
     },
+    scrollWidth: {
+      configurable: true,
+      get: () => scrollWidthSpy(),
+    },
   });
 
-  return { scrollHeightSpy };
+  return { scrollHeightSpy, scrollWidthSpy };
 }
 
 export function restoreScroll() {
@@ -20,6 +27,10 @@ export function restoreScroll() {
     scrollHeight: {
       configurable: true,
       value: scrollHeight,
+    },
+    scrollWidth: {
+      configurable: true,
+      value: scrollWidth,
     },
   });
 }
