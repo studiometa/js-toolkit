dec7a11bb1d7747d8f4e630c4454eaeed3918311
Titouan Mathis <titouan@studiometa.fr>
2024-04-11 10:01:03 +0200

    Fix tests

--- packages/tests/__utils__/matchMedia.ts
+++ packages/tests/__utils__/matchMedia.ts
@@ -1,11 +1,14 @@
 import { afterEach } from 'bun:test';
 import MatchMediaMock from 'jest-matchmedia-mock';
 
-export function useMatchMedia(mediaQuery = '(min-width: 80rem)') {
+const defaultMediaQuery = '(min-width: 80rem)';
+
+export function useMatchMedia(mediaQuery = defaultMediaQuery) {
   const matchMedia = new MatchMediaMock();
   matchMedia.useMediaQuery(mediaQuery);
 
   afterEach(() => {
+    matchMedia.useMediaQuery(defaultMediaQuery);
     matchMedia.destroy();
   });
 
