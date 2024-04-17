0fe9e7b5e491401cf58279963ce89dd8a526eb72
Titouan Mathis <titouan@studiometa.fr>
2024-04-17 20:29:06 +0200

    Add `on` and `off` alias for event binding

---
+++ packages/js-toolkit/utils/dom/event.ts
@@ -0,0 +1,46 @@
+function event(
+  mode: 'add',
+  target: EventTarget,
+  event: string,
+  callback: EventListenerOrEventListenerObject,
+  options?: boolean | AddEventListenerOptions,
+): void;
+function event(
+  mode: 'remove',
+  target: EventTarget,
+  event: string,
+  callback: EventListenerOrEventListenerObject,
+  options?: boolean | EventListenerOptions,
+): void;
+function event(
+  mode: 'add' | 'remove',
+  target: EventTarget,
+  event: string,
+  callback: EventListenerOrEventListenerObject,
+  options?: boolean | EventListenerOptions | AddEventListenerOptions,
+) {
+  target[`${mode}EventListener`](event, callback, options);
+}
+
+function on(
+  target: EventTarget,
+  event: string,
+  callback: EventListenerOrEventListenerObject,
+  options?: boolean | AddEventListenerOptions,
+): () => void;
+function on(...args: [EventTarget, string, EventListenerOrEventListenerObject]): () => void {
+  event('add', ...args);
+  return () => off(...args);
+}
+
+function off(
+  target: EventTarget,
+  event: string,
+  callback: EventListenerOrEventListenerObject,
+  options?: boolean | EventListenerOptions,
+): void;
+function off(...args: [EventTarget, string, EventListenerOrEventListenerObject]): void {
+  event('remove', ...args);
+}
+
+export { on, off };
