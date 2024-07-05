e1892b3dead7c4cd172f7cf20e29822764d3e488
Titouan Mathis <titouan@studiometa.fr>
2024-07-05 19:27:27 +0200

    Improve docs

--- packages/docs/.vitepress/theme/index.js
+++ packages/docs/.vitepress/theme/index.js
@@ -2,8 +2,9 @@ import DefaultTheme from 'vitepress/theme';
 import PreviewIframe from './components/PreviewIframe.vue';
 import './custom.scss';
 
+/** @type {import('vitepress').Theme} */
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('PreviewIframe', PreviewIframe);
   },
