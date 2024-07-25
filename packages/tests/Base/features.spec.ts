c47ea5e0c6c441307076e0e553ebfa9ea535ac29
Titouan Mathis <titouan@studiometa.fr>
2024-07-25 14:12:10 +0200

    Add tests

---
+++ packages/tests/Base/features.spec.ts
@@ -0,0 +1,65 @@
+import { describe, it, expect } from 'bun:test';
+import { Base, createApp } from '@studiometa/js-toolkit';
+import {
+  advanceTimersByTimeAsync,
+  h,
+  useFakeTimers,
+  useRealTimers,
+  mockFeatures,
+} from '#test-utils';
+import { features } from '#private/Base/features';
+
+describe('The configurable features', () => {
+  it('should allow configuration of different attributes', async () => {
+    const { unmock } = mockFeatures({
+      attributes: {
+        component: 'tk-is',
+        option: 'tk-opt',
+        ref: 'tk-ref',
+      },
+    });
+
+    const component = h('div', { id: 'component', tkIs: 'Foo' });
+    const ref = h('div', { id: 'btn', tkRef: 'btn' });
+    const div = h('div', { tkOptFoo: 'foo' }, [component, ref]);
+
+    class Foo extends Base {
+      static config = {
+        name: 'Foo',
+      };
+    }
+
+    interface AppProps {
+      $refs: {
+        btn: HTMLElement;
+      };
+      $children: {
+        Foo: Foo[];
+      };
+      $options: {
+        foo: string;
+      };
+    }
+
+    class App extends Base<AppProps> {
+      static config = {
+        name: 'App',
+        refs: ['btn'],
+        options: { foo: String },
+        components: {
+          Foo,
+        },
+      };
+    }
+
+    const app = new App(div);
+    useFakeTimers();
+    app.$mount();
+    await advanceTimersByTimeAsync(100);
+    useRealTimers();
+    expect(app.$options.foo).toBe(div.getAttribute('tk-opt-foo'));
+    expect(app.$refs.btn).toBe(ref);
+    expect(app.$children.Foo[0].$el).toBe(component);
+    unmock();
+  });
+});
