f37d5445356c355056a5365cb362a4f8e3bceab2
Titouan Mathis <titouan@studiometa.fr>
2024-07-25 14:12:10 +0200

    Add tests

--- packages/tests/__utils__/mockFeatures.ts
+++ packages/tests/__utils__/mockFeatures.ts
@@ -3,6 +3,11 @@ import { vi } from 'vitest';
 type Features = {
   blocking: boolean;
   breakpoints: Record<string, string>;
+  attributes: {
+    component: string;
+    option: string;
+    ref: string;
+  };
 };
 
 interface FeaturesMap extends Map<keyof Features, Features[keyof Features]> {
@@ -10,27 +15,43 @@ interface FeaturesMap extends Map<keyof Features, Features[keyof Features]> {
   set<T extends keyof Features>(key: T, value: Features[T]): this;
 }
 
+const defaultBreakpoints = {
+  xxs: '0rem',
+  xs: '30rem', // 480px
+  s: '48rem', // 768px
+  m: '64rem', // 1024px
+  l: '80rem', // 1280px
+  xl: '90rem', // 1440px
+  xxl: '120rem', // 1920px
+  xxxl: '160rem', // 2560px
+};
+
+const defaultAttributes = {
+  component: 'data-component',
+  option: 'data-option',
+  ref: 'data-ref',
+};
+
 export function mockFeatures({
   blocking = true,
-  breakpoints = {
-    xxs: '0rem',
-    xs: '30rem', // 480px
-    s: '48rem', // 768px
-    m: '64rem', // 1024px
-    l: '80rem', // 1280px
-    xl: '90rem', // 1440px
-    xxl: '120rem', // 1920px
-    xxxl: '160rem', // 2560px
-  },
+  breakpoints = defaultBreakpoints,
+  attributes = defaultAttributes,
 } = {}) {
   const features = new Map<keyof Features, Features[keyof Features]>([
     ['blocking', blocking],
     ['breakpoints', breakpoints],
+    ['attributes', attributes],
   ]) as FeaturesMap;
 
   vi.mock('#private/Base/features.js', () => {
     return { features };
   });
 
-  return { features };
+  function unmock() {
+    features.set('blocking', false);
+    features.set('breakpoints', defaultBreakpoints);
+    features.set('attributes', defaultAttributes);
+  }
+
+  return { features, unmock };
 }
