f8decc8dc959db5af17eb83970e7fe4aa4a8e592
Titouan Mathis <titouan@studiometa.fr>
2024-08-02 11:00:03 +0200

    Clean up tests

--- packages/tests/__utils__/getFilenamesInFolder.ts
+++
@@ -1,9 +0,0 @@
-import { resolve } from 'node:path';
-import fs from 'node:fs';
-
-export function getFilenamesInFolder(path) {
-  return  fs
-    .readdirSync(require.resolve(path))
-    .filter((file) => file !== 'index.js')
-    .map((file) => file.replace(/\.js$/, ''));
-}
