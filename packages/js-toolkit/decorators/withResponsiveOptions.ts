e6543c414f5dc52ff43cf9ab14bc27966055c8c0
Titouan Mathis <titouan@studiometa.fr>
2024-10-07 17:25:36 +0200

    Add support for defining responsive options in the decorator

--- packages/js-toolkit/decorators/withResponsiveOptions.ts
+++ packages/js-toolkit/decorators/withResponsiveOptions.ts
@@ -1,17 +1,37 @@
 import type { BaseInterface, BaseDecorator } from '../Base/types.js';
-import type { Base, BaseProps, Managers } from '../Base/index.js';
-import { ResponsiveOptionsManager } from '../Base/managers/ResponsiveOptionsManager.js';
+import type { Base, BaseConfig, BaseProps, Managers } from '../Base/index.js';
+import type { OptionObject, OptionType } from '../Base/managers/OptionsManager.js';
+import {
+  ResponsiveOptionsManager,
+  ResponsiveOptionObject,
+} from '../Base/managers/ResponsiveOptionsManager.js';
+import { isObject } from '../utils/index.js';
 
 /**
  * Extends the configuration of an existing class.
  */
 export function withResponsiveOptions<S extends Base>(
   BaseClass: typeof Base,
+  { responsiveOptions = [] } = {},
 ): BaseDecorator<BaseInterface, S> {
+  const options = BaseClass.config.options ?? {};
+  for (const option of responsiveOptions) {
+    const oldOption = options[option];
+    options[option] = {
+      type: isObject(oldOption) ? (oldOption as OptionObject).type : (oldOption as OptionType),
+      reponsive: true,
+    } as ResponsiveOptionObject;
+  }
+
   /**
    * Class.
    */
   class WithResponsiveOptions<T extends BaseProps = BaseProps> extends BaseClass<T> {
+    static config: BaseConfig = {
+      ...BaseClass.config,
+      options,
+    };
+
     /**
      * Get managers.
      */
