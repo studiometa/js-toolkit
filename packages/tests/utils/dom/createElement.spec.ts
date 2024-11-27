9c407a7d721e7fb8cfa8bb1e3cd4c41b763c0bd8
Titouan Mathis <titouan@studiometa.fr>
2024-11-27 18:58:02 +0100

    Lint files

--- packages/tests/utils/dom/createElement.spec.ts
+++ packages/tests/utils/dom/createElement.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { createElement } from '@studiometa/js-toolkit/utils';
 
 describe('The createElement function', () => {
