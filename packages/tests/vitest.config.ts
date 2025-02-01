a3c94815fcf5b7ccadc537105b1f131cfb394c3a
Titouan Mathis <titouan@studiometa.fr>
2025-02-01 12:59:34 +0100

    Disable happy-dom setup

--- packages/tests/vitest.config.ts
+++ packages/tests/vitest.config.ts
@@ -3,11 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     root: '..',
-    environment: 'happy-dom',
     alias: {
       '^#private/(.*)': '../js-toolkit/$1',
     },
-    setupFiles: ['./tests/__utils__/happydom.ts'],
     coverage: {
       provider: 'v8',
       include: ['js-toolkit/**'],
