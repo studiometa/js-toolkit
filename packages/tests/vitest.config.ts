248dca9efd833e319f72990e69c4d2a28096aff8
Titouan Mathis <titouan@studiometa.fr>
2025-02-01 12:59:04 +0100

    Disable screenshots

--- packages/tests/vitest.config.ts
+++ packages/tests/vitest.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
     browser: {
       provider: 'playwright',
       enabled: true,
+      screenshotFailures: false,
       instances: [{ browser: 'chromium' }],
     },
   },
