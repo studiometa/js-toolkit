0ead9592426b03231a70af5fb19f53224c4b0a96
Titouan Mathis <titouan@studiometa.fr>
2025-09-24 09:40:40 +0200

    Add chunking utils

---
+++ packages/js-toolkit/utils/array/chunks.ts
@@ -0,0 +1,19 @@
+/**
+ * Chunk an array into groups.
+ */
+export function chunks<T>(arr: T[], length = 1):T[][] {
+  const chunked = [];
+
+  // create chunks from arr.
+
+  return chunked;
+}
+
+
+/**
+ * Chunk an array into groups with overlapping sequences.
+ */
+export function slidingWindowChunks<T>(arr: T[], length = 1):Array<T[]> {
+  const chunked = [];
+  return chunked;
+}
